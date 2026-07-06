99c5b565d9386029e589445ffc3320ad1d1d3401
Weiyao Meng <50621731+weiyaomeng@users.noreply.github.com>
2026-07-06 10:41:27 +0000

    updated dates for testing purpose

--- analysis/preprocess_combine_gz.py
+++ analysis/preprocess_combine_gz.py
@@ -6,8 +6,8 @@ import argparse
 
 # utilisation: python analysis/preprocess_combine_gz.py
 
-start_dates = month_range(config.start, config.end)
-# start_dates = ["2024-02-01", "2024-03-01"]
+# start_dates = month_range(config.start, config.end)
+start_dates = ["2024-02-01", "2024-03-01"]
 
 dfs = []
 

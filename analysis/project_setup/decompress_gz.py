fb0ef4f5c3c487400483ae603f6fd738b8fd1add
Weiyao Meng <50621731+weiyaomeng@users.noreply.github.com>
2026-09-07 13:34:56 +0000

    updated decompress script

--- analysis/project_setup/decompress_gz.py
+++ analysis/project_setup/decompress_gz.py
@@ -1,15 +1,13 @@
-import argparse
 import gzip
 import shutil
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input", required=True)
-parser.add_argument("--output", required=True)
-args = parser.parse_args()
+INPUT_FILE = "output/dataset_patients_combined.csv.gz"
+OUTPUT_FILE = "output/dataset_patients_combined.csv"
 
-with gzip.open(args.input, "rb") as input_file:
-    with open(args.output, "wb") as output_file:
+
+with gzip.open(INPUT_FILE, "rb") as input_file:
+    with open(OUTPUT_FILE, "wb") as output_file:
         shutil.copyfileobj(input_file, output_file)
 
-print(f"Decompressed {args.input} to {args.output}")
+print(f"Decompressed {INPUT_FILE} to {OUTPUT_FILE}")
